e8b587c0938496c826c31466eb7c58837d6dd23c
Mercy <ariri691@gmail.com>
2025-07-17 15:26:36 +0300

    new

--- ClassX.py
+++ ClassX.py
@@ -1,3 +1,5 @@
 print("Hi Mercy , I am soo proud of you !! I am headed to be a double certified software engineer")
 print("This is only the start.")
 print("A journey of a thousand miles started in the year 2023...when you decided you could do this!")
+
+print("Th year is 2025...and we are still pushing!")
