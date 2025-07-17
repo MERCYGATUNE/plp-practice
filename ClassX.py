c526810ad14a50f535a37ffd6acd6c53da179f63
Mercy <ariri691@gmail.com>
2025-07-17 15:21:47 +0300

    added ClassX file and modified HTML FILE

---
+++ ClassX.py
@@ -0,0 +1,3 @@
+print("Hi Mercy , I am soo proud of you !! I am headed to be a double certified software engineer")
+print("This is only the start.")
+print("A journey of a thousand miles started in the year 2023...when you decided you could do this!")
